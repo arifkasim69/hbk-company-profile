96510b7181508cf0813dea494b387d6e1b0d9bea
Arif Kasim <36006556+arifkasim69@users.noreply.github.com>
2026-05-01 03:31:42 +0700

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -32,7 +32,7 @@ export function Home() {
             <img 
               alt="Structural Drawing" 
               className="w-full aspect-[4/5] object-cover grayscale hover:grayscale-0 transition-all duration-1000" 
-              src="razones-para-ir-a-un-resort-de-vacaciones-1.webp" 
+              src="unnamed.png" 
             />
           </motion.div>
 
